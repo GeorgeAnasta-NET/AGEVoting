bd040ca28fdb9a0109dd66297931a30d7d40e83b
George Anastasiadis <anastasiadisgeo87@gmail.com>
2021-10-20 22:46:48 +0300

    Creating some Models

---
+++ AGEVoting/Models/StandardModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AGEVoting.Models {
+    public class StandardModel {
+        public DateTime Created { get; set; }
+        public long CreatedBy { get; set; }
+
+        public DateTime Modified { get; set; }
+        public long ModifiedBy { get; set; }
+
+        public DateTime Deleted { get; set; }
+        public long DeletedBy { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
